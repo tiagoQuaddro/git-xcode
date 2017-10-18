a6dc73de31457a3e4ee5d59ef567a98c6c0d681e
Tiago Souza <tssmaquetes@yahoo.com.br>
2017-10-18 16:01:01 -0200

    Initial Commit

---
+++ git-xcode/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  git-xcode
+//
+//  Created by Tiago Souza on 17/10/2017.
+//  Copyright © 2017 Quaddro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK - Actions
+    @IBAction func showHello() {
+        
+        let alert = UIAlertController(title: "Hello World", message: "Integração do Git com o Xcode", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true)
+    }
+}
+
